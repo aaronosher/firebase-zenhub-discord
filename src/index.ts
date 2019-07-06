fdff8f80aeeb8fce532e5d13f334caede2c01a80
Aaron Osher <aaron@aaronosher.io>
2019-07-07 00:54:07 +0100

    Moved issue transfer to its own file

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,30 @@
-import * as functions from 'firebase-functions';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { issueTransfer } from './webhookHandlers';
+import Webhook from './types/Webhook';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}));
 
-// require('dotenv').config()
+app.post('/', async (req: Request, res: Response) => {
+  const body = req.body as Webhook;
+  
+  try {
+    if(body.type === 'issue_transfer') {
+      await issueTransfer(body);
+      return res.json({
+        success: true,
+        error: false,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      error: "an error occured",
+    });
+  }
+});
 
-// const express = require('express')
-// var bodyParser = require('body-parser');
-
-// const app = express()
-
-// var issueTransfer = require('./lib/issue-transfer')
-
-// app.set('port', (process.env.PORT || 3000));
-
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
-
-// app.post('/', function(req, res) {
-//   if(req.body.type === 'issue_transfer') {
-//     issueTransfer.sendDiscordMessage(req.body)
-//   }
-// })
-
-// app.listen(app.get('port'), function () {
-//   console.log('Example app listening on port 3000!')
-// })
+export default app;
