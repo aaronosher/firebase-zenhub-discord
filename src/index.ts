d67f4d50c7ad60733dedca233ae13f6565c2ed50
Aaron Osher <aaron@aaronosher.io>
2019-07-07 00:13:09 +0100

    Add firebase

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+
+// require('dotenv').config()
+
+// const express = require('express')
+// var bodyParser = require('body-parser');
+
+// const app = express()
+
+// var issueTransfer = require('./lib/issue-transfer')
+
+// app.set('port', (process.env.PORT || 3000));
+
+// app.use(bodyParser.urlencoded({extended: true}));
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+// app.post('/', function(req, res) {
+//   if(req.body.type === 'issue_transfer') {
+//     issueTransfer.sendDiscordMessage(req.body)
+//   }
+// })
+
+// app.listen(app.get('port'), function () {
+//   console.log('Example app listening on port 3000!')
+// })
