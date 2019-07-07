ba06f17e197fbd5c0139dd35acf1f7b093ac3ae6
Aaron Osher <aaron@aaronosher.io>
2019-07-07 03:51:58 +0100

    Updated to use best practice for stringcat

--- src/webhookHandlers/issueTransfer.ts
+++ src/webhookHandlers/issueTransfer.ts
@@ -3,7 +3,7 @@ import sendHook from '../utils/sendHook';
 import composeWebhook from '../utils/composeWebhook';
 
 const sendDiscordMessage = (webhook: IssueTransfer) => {
-  const description = 'Moved from "' + webhook.from_pipeline_name + '" to "' + webhook.to_pipeline_name + '"';
+  const description = `Moved from "${webhook.from_pipeline_name}" to "${webhook.to_pipeline_name}"`;
   const message = composeWebhook(webhook, description);
 
   return sendHook(message);
