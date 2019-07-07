d818340145f183fbcc2d439cc5b4782f62c14b6a
Aaron Osher <aaron@aaronosher.io>
2019-07-07 03:08:39 +0100

    Make send hook it's own file

--- src/webhookHandlers/issueTransfer.ts
+++ src/webhookHandlers/issueTransfer.ts
@@ -1,16 +1,16 @@
-import { config } from 'firebase-functions';
-import fetch, { RequestInit } from 'node-fetch';
 import IssueTransfer from '../types/Webhook/IssueTransfer';
+import sendHook from '../utils/sendHook';
+import WebhookRequest from '../types/Discord/WebhookRequest';
 
 const sendDiscordMessage = (webhook: IssueTransfer) => {
-  const message = {
+  const message: WebhookRequest = {
     username: 'Zenhub',
     embeds: [
       {
         title: '#' + webhook.issue_number + ' : ' + webhook.issue_title,
         description: 'Moved from "' + webhook.from_pipeline_name + '" to "' + webhook.to_pipeline_name + '"',
         url: webhook.github_url,
-        color: '0153170', 
+        color: 0x00d090, 
         author: {
           name: webhook.user_name,
           icon_url: 'https://avatars.githubusercontent.com/' + webhook.user_name,
@@ -22,15 +22,7 @@ const sendDiscordMessage = (webhook: IssueTransfer) => {
     ],
   };
 
-  const postOptions: RequestInit = {
-    method: 'post',
-    body: JSON.stringify(message),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  return fetch(config().discord.webhook, postOptions);
+  return sendHook(message);
 };
 
 export default sendDiscordMessage;
