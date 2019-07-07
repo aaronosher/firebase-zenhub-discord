e627f79336f3d704ee1a413af6cdfcb0c9d4b1b9
Aaron Osher <aaron@aaronosher.io>
2019-07-07 03:15:44 +0100

    Move webhook message generation to it's own function

--- src/webhookHandlers/issueTransfer.ts
+++ src/webhookHandlers/issueTransfer.ts
@@ -1,26 +1,10 @@
 import IssueTransfer from '../types/Webhook/IssueTransfer';
 import sendHook from '../utils/sendHook';
-import WebhookRequest from '../types/Discord/WebhookRequest';
+import composeWebhook from '../utils/composeWebhook';
 
 const sendDiscordMessage = (webhook: IssueTransfer) => {
-  const message: WebhookRequest = {
-    username: 'Zenhub',
-    embeds: [
-      {
-        title: '#' + webhook.issue_number + ' : ' + webhook.issue_title,
-        description: 'Moved from "' + webhook.from_pipeline_name + '" to "' + webhook.to_pipeline_name + '"',
-        url: webhook.github_url,
-        color: 0x00d090, 
-        author: {
-          name: webhook.user_name,
-          icon_url: 'https://avatars.githubusercontent.com/' + webhook.user_name,
-        },
-        footer: {
-          text: webhook.organization + '/' + webhook.repo,
-        },
-      },
-    ],
-  };
+  const description = 'Moved from "' + webhook.from_pipeline_name + '" to "' + webhook.to_pipeline_name + '"';
+  const message = composeWebhook(webhook, description);
 
   return sendHook(message);
 };
